e3e366628b4cf3cc473b9b7b3912dc8821b03a3d
Root99 <azizrezgui4@gmail.com>
2023-08-04 15:02:46 +0100

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className={"bg-black h-[100vh]"}>
+      <h1 className={" text-3xl text-green-500 font-bold"}>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
