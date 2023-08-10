4b5476c682c53faa0edeecc8c7bd1cce85b0f572
Root99 <azizrezgui4@gmail.com>
2023-08-10 02:41:49 +0100

    refactoring

---
+++ src/components/pages/Home/SinglePost/SinglePostExcerpt.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { EntityId } from "@reduxjs/toolkit";
+import { useAppSelector } from "../../../../app/hooks.ts";
+import { selectPostById } from "../../../../features/post/postsSlice.ts";
+import { Icon } from "../../../shared/Icon.tsx";
+import { BiDotsVertical, BiSave, BiTime } from "react-icons/bi";
+import { TimeAgo } from "../../../shared/TimeAgo.tsx";
+
+export const SinglePostExcerpt = React.memo(
+  ({ postID }: { postID: EntityId }) => {
+    const post = useAppSelector((state) => selectPostById(state, postID));
+
+    if (!post) return null;
+    return (
+      <div
+        className={
+          "mx-auto my-4 w-full  min-w-[420px] max-w-[40%] rounded-md border border-borderGray bg-mainDark p-4"
+        }
+      >
+        {/*  User*/}
+
+        <div className={"flex items-center justify-between gap-[18px]"}>
+          <div className={"flex items-center gap-[18px]"}>
+            <div
+              className={"h-[40px]  w-[40px] rounded-full bg-blue-400"}
+            ></div>
+            <div>
+              <h1 className={"text-white"}>{post.title}</h1>
+              <div className={"flex items-center gap-x-1"}>
+                <Icon icon={BiTime} className={"text-textGray"} />
+                <TimeAgo timestamp={post.created_at} />
+              </div>
+            </div>
+          </div>
+
+          <div className={"flex items-center gap-x-4"}>
+            <Icon icon={BiSave} className={"text-[20px] text-white"} />
+            <Icon icon={BiDotsVertical} className={"text-[20px] text-white"} />
+          </div>
+        </div>
+
+        <p className={"my-3 text-sm font-normal text-white"}>{post?.body}</p>
+      </div>
+    );
+  }
+);
