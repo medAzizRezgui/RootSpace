f0fc683b29a4ba94bff09b1ed21bf35ff4af5a2c
Root99 <azizrezgui4@gmail.com>
2023-08-10 15:30:44 +0100

    wip: profile form

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -1,17 +1,15 @@
 import { openModal } from "../../features/user/authModalSlice.ts";
 import AuthModal from "../../features/user/AuthModal.tsx";
 import { useAppDispatch } from "../../app/hooks.ts";
-import { useUser } from "../../hooks/useUser.ts";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useNavigate } from "react-router-dom";
+import { ProfileForm } from "../../components/pages/Account/ProfileForm";
 
 const Account = () => {
   const dispatch = useAppDispatch();
 
   const supabaseClient = useSupabaseClient();
-  const { user, userDetails } = useUser();
   const router = useNavigate();
-
   const handleRefresh = () => {
     router("", { replace: true });
   };
@@ -20,26 +18,28 @@ const Account = () => {
     handleRefresh();
   };
   return (
-    <div className={"h-[100vh] bg-bgDark p-24 font-display text-white"}>
-      <button
-        onClick={() => dispatch(openModal())}
-        className={"rounded-md bg-green-800 p-4 font-medium text-white"}
-      >
-        Open Modal
-      </button>
-      <AuthModal />
-      <p className={"font-medium text-white"}>
-        {user ? "Logged In" : "Logged Out"}
-      </p>
-      <p className={"font-medium text-white"}>
-        {userDetails ? userDetails.full_name : "Logged Out"}
-      </p>
-      <button
-        onClick={() => handleLogout()}
-        className={"rounded-md bg-red-500 p-4 text-white"}
-      >
-        Logout
-      </button>
+    <div
+      className={
+        "flex h-[100vh] items-start bg-bgDark p-24 font-display text-white"
+      }
+    >
+      <>
+        <button
+          onClick={() => dispatch(openModal())}
+          className={"rounded-md bg-green-800 p-4 font-medium text-white"}
+        >
+          Open Modal
+        </button>
+        <AuthModal />
+
+        <button
+          onClick={() => handleLogout()}
+          className={"rounded-md bg-red-500 p-4 text-white"}
+        >
+          Logout
+        </button>
+      </>
+      <ProfileForm />
     </div>
   );
 };
