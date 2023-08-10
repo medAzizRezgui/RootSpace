0ffc0be824e3122f4d55b476c92cd0d3877bc5e0
Root99 <azizrezgui4@gmail.com>
2023-08-10 02:49:27 +0100

    refactoring

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -1,25 +1,6 @@
-import { useUser } from "../../hooks/useUser.ts";
-import { supabase } from "../../libs/supabaseClient.ts";
-
 const Account = () => {
-  const { userDetails, isLoading } = useUser();
-
-  const updateDetails = async () => {
-    await supabase
-      .from("profiles")
-      .update({ full_name: "Rezgui med Aziz", username: "Rez99" })
-      .eq("id", userDetails?.id)
-      .select();
-  };
-  if (isLoading) return <h1>Loading...</h1>;
   return (
-    <div className={"h-[100vh] bg-bgDark p-24 font-display text-white"}>
-      <h1>Account Details :</h1>
-      <p>FullName: {userDetails?.full_name}</p>
-      <p>Username: {userDetails?.username}</p>
-
-      <button onClick={() => updateDetails()}>Update</button>
-    </div>
+    <div className={"h-[100vh] bg-bgDark p-24 font-display text-white"}></div>
   );
 };
 
