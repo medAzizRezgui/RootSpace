d2a7eae50c32f19afe7d170d8ba88b279ba41d8b
Root99 <azizrezgui4@gmail.com>
2023-08-18 20:27:32 +0100

    wip: image support for posts

--- src/components/pages/Home/AddPost/AddPost.tsx
+++ src/components/pages/Home/AddPost/AddPost.tsx
@@ -1,10 +1,4 @@
-import {
-  BiCamera,
-  BiEditAlt,
-  BiImage,
-  BiLocationPlus,
-  BiSmile,
-} from "react-icons/bi";
+import { BiEditAlt, BiImage, BiLocationPlus, BiSmile } from "react-icons/bi";
 import { AiOutlinePaperClip } from "react-icons/ai";
 import { Icon } from "../../../shared/Icon.tsx";
 import Button from "../../../shared/Button.tsx";
@@ -14,22 +8,51 @@ import { addNewPost } from "../../../../features/post/postsSlice.ts";
 import { useUser } from "../../../../hooks/useUser.ts";
 import useLoadImage from "../../../../hooks/useLoadImage.ts";
 import { twMerge } from "tailwind-merge";
+import Input from "../../../shared/Input.tsx";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import uniqid from "uniqid";
 export const AddPost = () => {
   const dispatch = useAppDispatch();
   const { user, userDetails } = useUser();
   const avatarUrl = useLoadImage(userDetails);
   const [addRequestStatus, setAddRequestStatus] = useState("idle");
   const [content, setContent] = useState("");
+
+  const [postPicture, setPostPicture] = useState<File | null>(null);
   const onContentChanged = (e: React.ChangeEvent<HTMLInputElement>) =>
     setContent(e.target.value);
   const canSave = addRequestStatus === "idle";
+
+  const supabaseClient = useSupabaseClient();
+
   const onPostClicked = async () => {
     if (!canSave) return;
     try {
       setAddRequestStatus("pending");
-      await dispatch(addNewPost({ body: content })).unwrap();
+      if (postPicture) {
+        console.log(postPicture);
+        const id = uniqid();
+        const { error: avatarError, data } = await supabaseClient.storage
+          .from("post_pictures")
+          .upload(`post--${id}`, postPicture, {
+            cacheControl: "3600",
+            upsert: false,
+          });
 
-      setContent("");
+        if (avatarError) {
+          console.log("ERROR", avatarError.message);
+          return;
+        }
+        await dispatch(
+          addNewPost({ body: content, img_url: data?.path })
+        ).unwrap();
+        setContent("");
+      }
+
+      if (!postPicture) {
+        await dispatch(addNewPost({ body: content })).unwrap();
+        setContent("");
+      }
     } catch (_) {
       console.error("Failed to post");
     } finally {
@@ -63,7 +86,7 @@ export const AddPost = () => {
           value={content}
           onChange={onContentChanged}
           className={
-            "h-[40px] flex-1 select-none rounded-[20px] bg-mainGray px-4 placeholder:text-textGray focus:outline-none"
+            "h-[40px] flex-1 select-none rounded-[20px] bg-mainGray px-4 text-white placeholder:text-textGray focus:outline-none"
           }
         />
       </div>
@@ -75,8 +98,24 @@ export const AddPost = () => {
             "flex items-center justify-between gap-x-4 text-[28px] text-textGray"
           }
         >
-          <Icon icon={BiCamera} />
-          <Icon icon={BiImage} />
+          <div className={"relative  rounded-full"}>
+            <Icon icon={BiImage} />
+            <Input
+              className={
+                "absolute top-0 left-0  z-[100]  cursor-pointer select-none rounded-full opacity-0"
+              }
+              onChange={(e) =>
+                setPostPicture(e.target.files ? e.target.files[0] : null)
+              }
+              id={"image"}
+              type={"file"}
+              disabled={false}
+              accept={"image/*"}
+            />
+          </div>
+
+          {/*<Icon icon={BiCamera} />*/}
+
           <Icon icon={AiOutlinePaperClip} />
           <Icon icon={BiLocationPlus} />
           <Icon icon={BiSmile} />
