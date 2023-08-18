22506e68797b7ceab94c85c4d7157612c17e3814
Root99 <azizrezgui4@gmail.com>
2023-08-18 04:23:49 +0100

    wip: reaction system

---
+++ src/utils/functions.ts
@@ -0,0 +1,22 @@
+import { Like } from "./types/types.ts";
+
+export function isIdInArray<T extends Like>(
+  idToCheck: string | undefined,
+  array: T[]
+): boolean {
+  return array.some((obj) => obj.user_id === idToCheck);
+}
+function capitalizeFirstLetter(word: string | undefined) {
+  if (word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  } else return "";
+}
+
+export const fullName = (
+  firstName: string | undefined,
+  lastName: string | undefined
+) => {
+  return (
+    capitalizeFirstLetter(lastName) + " " + capitalizeFirstLetter(firstName)
+  );
+};
